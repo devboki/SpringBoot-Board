aff0c7b6d6ed78680ed4080be064373f3a271465
devboki <octsixxth@gmail.com>
2021-10-21 16:39:46 +0900

    add comment (CR)

--- src/main/java/com/board/domain/CommonDTO.java
+++ src/main/java/com/board/domain/CommonDTO.java
@@ -5,9 +5,15 @@ import com.board.paging.PaginationInfo;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class CommonDTO extends Criteria {
 
     private PaginationInfo paginationInfo;
+    private String deleteYn;
+    private LocalDateTime insertTime;
+    private LocalDateTime updateTime;
+    private LocalDateTime deleteTime;
 }
